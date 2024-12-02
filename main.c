b9f246e75b1baf28577315cd0c6a7b2732524a2e
dudasdominik <dudasdominik3@gmail.com>
2024-12-02 20:00:27 +0100

    small adjustment to decodefile and started terminal manipulation

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <locale.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <uchar.h>
-#include <wchar.h>
+#include <unistd.h>
 
 
 #define MAX_LINES 100
 #define MAX_LINE_LENGTH 800
+#define MAX_ENTRIES 1600
+#define PAGE_SIZE 10
+
 char lines[MAX_LINES][MAX_LINE_LENGTH];
 char username[800];
 char password[800];
 int key[] = {5, -14, 31, -9, 3};
 int key_length = 5;
-char decoded_data[1600];
+char users[1600];
+
+typedef struct {
+    char name[50];
+    char phone[50];
+} PhonebookEntry;
+
+char phonebook_data[MAX_ENTRIES];
+
+PhonebookEntry phonebook[800];
+
 
 void getinputs () {
     printf("\nPlease enter the username: \n");
@@ -28,49 +41,29 @@ void decode_file(char *filename) {
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         printf("Error: File not found\n");
+        return;
     }
+        int isuser = (strcmp(filename, "D:/Work/untitled/telefonkonyv.bin") == 0) ? 0 : 1;
         int byte;
         int i = 0;
         int j = 0;
-        int prev_byte = -1;
     while ((byte = fgetc(file)) != EOF) {
         if (byte != 0x0A) {
             byte -= key[i];
             i = (i + 1) % key_length;
-            printf("----------------------\n");
-            printf("BYTE: %d\n", byte);
-            printf("CHAR: %c\n", (char) byte);
-            printf("\n-------------------");
-            if((byte == 195 && prev_byte == 179) || (byte == 179 && prev_byte == 195)) {
-                decoded_data[j++] = 162;
-                prev_byte = -1;
-            }else if(byte == 161) {
-                decoded_data[j++] = 160;
-            }else if(byte == 182) {
-                decoded_data[j++] = 148;
-            }else if(byte == 195 || byte == 179) {
-                prev_byte = byte;
-                continue;
-            }
-            else if (prev_byte != -1) {
-                decoded_data[j++] = (char) byte;
-                prev_byte = byte;
-            }else {
-                prev_byte = byte;
-                decoded_data[j++] = (char) byte;
-            }
+            isuser == 1 ? (users[j++] = (char)byte) : (phonebook_data[j++] = (char)byte);
         } else {
             i = 0;
-            //decoded_data[j++] = '\n';
+            isuser == 1 ? (users[j++] = '\n') : (phonebook_data[j++] = '\n');
         }
     }
-    decoded_data[j] = '\0';
+    isuser == 1 ? (users[j++] = '\0') : (phonebook_data[j++] = '\0');
     fclose(file);
 }
 
-void split_username_password() {
+bool split_username_password() {
     int line_count = 0;
-    char *line = strtok(decoded_data, "\n");
+    char *line = strtok(users, "\n");
 
     while (line != NULL && line_count < MAX_LINES) {
         strncpy(lines[line_count], line, MAX_LINE_LENGTH - 1);
@@ -86,43 +79,75 @@ void split_username_password() {
             char *pass = test + 1;
             if ((strcmp(password, pass) == 0) && (strcmp(username, user) == 0)) {
                 printf("Login successful\n");
-                return;
+                return true;
             }
         }
     }
     printf("Login failed\n");
+    return false;
 }
 
-
-void username_to_bytes(const char *username, unsigned char *byte_array) {
-    size_t length = strlen(username);
-    for (size_t i = 0; i < length; i++) {
-        byte_array[i] = (unsigned char)username[i];
+void split_phonebook() {
+    char *line = strtok(phonebook_data, "\n");
+    int entry_count = 0;
+    while (line != NULL) {
+        char *separator = strchr(line, ';');
+        if ((separator != NULL) && (entry_count < MAX_ENTRIES)) {
+            *separator = '\0';
+            strncpy(phonebook[entry_count].name, line, sizeof(phonebook[entry_count].name) - 1);
+            strncpy(phonebook[entry_count].phone, separator + 1, sizeof(phonebook[entry_count].phone) - 1);
+            phonebook[entry_count].name[sizeof(phonebook[entry_count].name) - 1] = '\0';
+            phonebook[entry_count].phone[sizeof(phonebook[entry_count].phone) - 1] = '\0';
+            entry_count++;
+        }
+        line = strtok(NULL, "\n");
     }
-    byte_array[length] = '\0';
 }
 
+void draw_rectangle(int width, int height) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+                printf("#");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void display_phonebook_page(int page) {
+    int start_index = page * PAGE_SIZE;
+    int end_index = start_index + PAGE_SIZE;
+    if (end_index > MAX_ENTRIES) {
+        end_index = MAX_ENTRIES;
+    }
+
+    draw_rectangle(60, 15);
+    printf("\nTelefonkönyv - %d. oldal\n", page + 1);
+    for (int i = start_index; i < end_index && phonebook[i].name[0] != '\0'; i++) {
+        printf("%d. %s - %s\n", i + 1, phonebook[i].name, phonebook[i].phone);
+    }
+    printf("\nNavigálj a következő oldalakhoz: [n] Következő, [p] Előző, [q] Kilépés\n");
+}
 
 
 int main() {
     setlocale(LC_ALL, "hu_HU.UTF-8");
-    //getinputs();
-    decode_file("D:/Work/untitled/telefonkonyv.bin");
-    //split_username_password();
-    printf(decoded_data);
+    getinputs();
+    decode_file("D:/Work/untitled/jelszo_javitott.bin");
+    if(split_username_password()) {
+        decode_file("D:/Work/untitled/telefonkonyv.bin");
+        split_phonebook();
+        display_phonebook_page(2);
+        sleep(120);
+    }
+
 
     /*
-    unsigned char byte_array[800];
-    username_to_bytes(username, byte_array);
-    printf("----------------------\n");
-    for (size_t i = 0; i < strlen((char *)byte_array); i++) {
-        printf("%d ", byte_array[i]);
-        printf("CHAR: %c\n", (char) byte_array[i]);
-    }
-    printf("\n");
-    */
-
-
-
+    for (int i = 0; i < 800 && phonebook[i].name[0] != '\0'; i++) {
+        printf("%s - %s\n", phonebook[i].name, phonebook[i].phone);
+    } */
     return 0;
 }
